9c2876f8713a5200066427761a86a60f50b709a5
Caravaggio <kcarava@kih.kmart.com>
2016-11-13 20:15:14 -0600

    dev versioning

--- src/test/java/com/boomerang/test/core/RestfulTest.java
+++ src/test/java/com/boomerang/test/core/RestfulTest.java
@@ -10,19 +10,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import com.boomerang.core.Application;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes=Application.class)
 @AutoConfigureMockMvc
 public class RestfulTest {
     private static final String PING = "OK";
 
     @Autowired
     private MockMvc mvc;
-
+    
     @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/ping").accept(MediaType.TEXT_PLAIN))
